969ec042f6d7da434fd4b45cec61125a9ccd7893
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2023-03-08 14:48:00 +0100

    feat: complete the conclusion part of the get started page

--- src/Components/GetStarted/GetStarted.tsx
+++ src/Components/GetStarted/GetStarted.tsx
@@ -55,7 +55,7 @@ function GettingStarted({ toggleTheme }: toggleProps) {
           className="photo-heading"
           color={toggleTheme ? theme.home_bg : theme.app_text_dark_big}
         >
-          2.Choose the template to copy
+          2. Choose the template to copy
         </Article>
         <ImageContainer>
           <img src={clicktocopy} alt="first image description" />
@@ -64,11 +64,41 @@ function GettingStarted({ toggleTheme }: toggleProps) {
           className="photo-heading"
           color={toggleTheme ? theme.home_bg : theme.app_text_dark_big}
         >
-          2.Click copy to copy code snippet
+          3. Click copy to copy code snippet
         </Article>
         <ImageContainer>
           <img src={clicktocopy} alt="first image description" />
         </ImageContainer>
+        <SubHeading> Conclusion</SubHeading>
+        <Article color={toggleTheme ? theme.home_bg : theme.app_text_dark_big}>
+          To use this feature, users can simply navigate to the component they
+          want to use, view the code snippet, and click on the{" "}
+          <span>"Copy"</span> button next to it. The code will be automatically
+          copied to their clipboard, and they can then paste it into their code
+          editor or IDE.
+        </Article>
+        <Article
+          className="para"
+          color={toggleTheme ? theme.home_bg : theme.app_text_dark_big}
+        >
+          It's important to note that the "Copy" button relies on the Clipboard
+          API, which is supported in modern browsers such as Google Chrome,
+          Mozilla Firefox, Microsoft Edge, and Safari. If a user's browser does
+          not support this API, they can still copy the code manually by
+          selecting it and using the copy function in their browser or
+          operating system.
+        </Article>
+        <Article
+          className="para"
+          color={toggleTheme ? theme.home_bg : theme.app_text_dark_big}
+        >
+          In conclusion, LazyFix is a useful web application for frontend
+          developers looking for reusable UI components. Built using React,
+          TypeScript, and Styled Components. The project is open source, so
+          contributions are welcome. To get started with the project, simply
+          clone the repository from GitHub and follow the instructions in
+          the README file.
+        </Article>
       </SubContainer>
     </Container>
   );
@@ -77,7 +107,7 @@ function GettingStarted({ toggleTheme }: toggleProps) {
 export default GettingStarted;
 
 const SubContainer = styled.main`
-  width: 80%;
+  width: 90%;
   margin-left: 50px;
   .para {
     margin-top: 10px;
